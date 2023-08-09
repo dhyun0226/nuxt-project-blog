6bb291eb7fb307255ed2161c127112a8670ff180
LeeDonghyun <donghyun.lee@osstem.com>
2023-08-09 16:52:41 +0900

    Initial commit

---
+++ app.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>dl</div>
+  <ClientOnly>
+    <UButton
+      :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+      color="gray"
+      variant="ghost"
+      aria-label="Theme"
+      @click="isDark = !isDark"
+    />
+
+    <template #fallback>
+      <div class="w-8 h-8" />
+    </template>
+  </ClientOnly>
+  <UAccordion color="yellow" variant="outline" size="2xs" />
+  <UAccordion color="yellow" variant="outline" size="2xs" />
+
+  <UAccordion :items="items">
+    <template #item="{ item }">
+      <p class="italic text-gray-900 dark:text-white text-center">
+        {{ item.description }}
+      </p>
+    </template>
+
+    <template #getting-started>
+      <div class="flex flex-col justify-center items-center gap-1">
+        <NuxtLink
+          to="/getting-started"
+          class="flex items-end gap-1.5 font-bold text-xl text-gray-900 dark:text-white"
+        >
+          <Logo class="w-8 h-8 text-primary-500 dark:text-primary-400" />
+
+          <span class="hidden sm:block">NuxtLabs</span
+          ><span class="sm:text-primary-500 dark:sm:text-primary-400">UI</span>
+        </NuxtLink>
+
+        <p class="text-sm text-gray-500 dark:text-gray-400">
+          Fully styled and customizable components for Nuxt.
+        </p>
+      </div>
+    </template>
+
+    <template #installation="{ description }">
+      <div class="flex flex-col justify-center items-center gap-1 mb-4">
+        <h3 class="text-xl font-bold text-gray-900 dark:text-white">
+          Installation
+        </h3>
+        <p class="text-sm text-gray-500 dark:text-gray-400">
+          Install <code>@nuxthq/ui</code> dependency to your project:
+        </p>
+        <p>
+          {{ description }}
+        </p>
+      </div>
+
+      <div class="flex flex-col items-center">
+        <code>$ npm install @nuxtlabs/ui</code>
+        <code>$ nnpm install -D @nuxthq/ui</code>
+        <code>$ pnpm i -D @nuxthq/ui</code>
+      </div>
+    </template>
+  </UAccordion>
+</template>
+
+<script setup>
+const colorMode = useColorMode();
+
+const isDark = computed({
+  get() {
+    return colorMode.value === "dark";
+  },
+  set() {
+    colorMode.preference = colorMode.value === "dark" ? "light" : "dark";
+  },
+});
+
+const items = [
+  {
+    label: "Getting Started",
+    icon: "i-heroicons-information-circle",
+    defaultOpen: true,
+    slot: "getting-started",
+  },
+  {
+    label: "Installation",
+    icon: "i-heroicons-arrow-down-tray",
+    defaultOpen: true,
+    slot: "installation",
+  },
+  {
+    label: "Theming",
+    icon: "i-heroicons-eye-dropper",
+    defaultOpen: true,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque elit, tristique placerat feugiat ac, facilisis vitae arcu. Proin eget egestas augue. Praesent ut sem nec arcu pellentesque aliquet. Duis dapibus diam vel metus tempus vulputate.",
+  },
+];
+import { getPageTable } from "vue-notion";
+
+getPageTable("Blog-ec018805f467435ab074c1b80c1f6e96").then((b) => {
+  console.log(b);
+});
+
+</script>
---
+++ pages/icopy.vue
@@ -0,0 +1,21 @@
+<script setup>
+// https://stupendous-okra-b7e.notion.site/Blog-ec018805f467435ab074c1b80c1f6e96?pvs=4
+
+const route = useRoute();
+
+let table = getPageTable("Blog-ec018805f467435ab074c1b80c1f6e96").then((b) => {
+  console.log(b);
+});
+
+</script>
+
+<template>
+  <div>
+    <h1>Nuxt Routing set up successfully!</h1>
+    <p>Current route: {{ route.path }}</p>
+    <a href="https://nuxt.com/docs/getting-started/routing" target="_blank"
+      >Learn more about Nuxt Routing</a
+    >
+  </div>
+  <div>{{ table }}</div>
+</template>
